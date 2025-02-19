2cff1ae53efa64df0785e35d2cebce60aa7332e2
gouravkotnala <gouravkotnala777@gmail.com>
2025-02-19 16:40:00 +0530

    feat(utilFunctions.ts) : add 'getMonthsCovered' function to get covered time

--- src/utils/utilFunctions.ts
+++ src/utils/utilFunctions.ts
@@ -20,3 +20,15 @@ export const sendToken = async(
             next(error);
         }
 };
+
+export const getMonthsCovered = (createdAt?:Date) => {
+    if (!createdAt)return 0;
+
+    const startingDate = new Date(createdAt);
+    const currentDate = new Date();
+
+    const yearDiff = currentDate.getFullYear() - startingDate.getFullYear();
+    const monthDiff = currentDate.getMonth() - startingDate.getMonth();
+
+    return yearDiff*12 + monthDiff + 1;
+};
