53651c7bba75f59228095cc6bc6f1b7c62bda3b6
gouravkotnala <gouravkotnala777@gmail.com>
2025-03-02 17:50:35 +0530

    feat(siteModel.ts) : add 'soldArea' field

--- src/models/siteModel.ts
+++ src/models/siteModel.ts
@@ -4,8 +4,10 @@ export interface SiteTypes{
     _id:ObjectId;
     siteName:string;
     totalSize:number;
+    soldArea:number;
 };
 export type CreateSiteBodyTypes = Pick<SiteTypes, "siteName"|"totalSize">;
+export type UpdateSiteBodyTypes = Partial<Pick<SiteTypes, "totalSize"|"soldArea">>&{siteID:ObjectId;};
 
 const siteSchema = new mongoose.Schema<SiteTypes>({
     siteName:{
@@ -13,7 +15,11 @@ const siteSchema = new mongoose.Schema<SiteTypes>({
         lowercase:true,
         required:true
     },
-    totalSize:Number
+    totalSize:Number,
+    soldArea:{
+        type:Number,
+        default:0
+    }
 }, {timestamps:true});
 
 const siteModel:Model<SiteTypes> = mongoose.models.Site || mongoose.model<SiteTypes>("Site", siteSchema);
