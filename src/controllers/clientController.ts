1a9f078fbff6c5ed3589d9d7ee3087035ba5f10b
gouravkotnala <gouravkotnala777@gmail.com>
2025-04-21 14:42:50 +0530

    feat(clientRoutes.ts & clientController.ts) : add add controller to send client message

--- src/controllers/clientController.ts
+++ src/controllers/clientController.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import Client, { CreateClientBodyTypes } from "../models/clientModel";
 import { ErrorHandler } from "../utils/utilClasses";
 import Slip from "../models/slipModel";
+import { sendSMS } from "../utils/utilFunctions";
 
 
 // Get all clients by admin
@@ -63,6 +64,24 @@ export const createClient = async(req:Request, res:Response, next:NextFunction) 
         });
 
         res.status(200).json({success:true, message:"Client created", jsonData:newClient});
+    } catch (error) {
+        console.log(error);
+        next(error);
+    }
+};
+export const sendMessageToClient = async(req:Request, res:Response, next:NextFunction) => {
+    try {
+        const {to, message} = req.body;
+
+        if (!to || !message) return next(new ErrorHandler("All fields are required", 400));
+
+        const formatedMessage = `${message}`
+
+        const sendMessage = await sendSMS(to, formatedMessage, next);
+
+        if (!sendMessage)  return next(new ErrorHandler("Something went wrong", 500));
+
+        res.status(200).json({success:true, message:"message has been sent", jsonData:{}});
     } catch (error) {
         console.log(error);
         next(error);
--- src/routers/clientRoutes.ts
+++ src/routers/clientRoutes.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { isUserAuthenticated } from "../middlewares/auth";
-import { cancelClent, createClient, findAllClients, findSingleClient, findSingleClientAllSlips } from "../controllers/clientController";
+import { cancelClent, createClient, findAllClients, findSingleClient, findSingleClientAllSlips, sendMessageToClient } from "../controllers/clientController";
 
 const clientRouter = express.Router();
 
@@ -8,6 +8,7 @@ clientRouter.route("/all-clients").get(isUserAuthenticated, findAllClients);
 clientRouter.route("/single-client2").get(isUserAuthenticated, findSingleClient);
 clientRouter.route("/single-client").get(isUserAuthenticated, findSingleClientAllSlips);
 clientRouter.route("/create").post(isUserAuthenticated, createClient);
+clientRouter.route("/send-message").post(isUserAuthenticated, sendMessageToClient);
 clientRouter.route("/cancel").delete(isUserAuthenticated, cancelClent);
 
 export default clientRouter;
