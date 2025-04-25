c917962b53e0b8009a9304ee44ed48b367f2f531
gouravkotnala <gouravkotnala777@gmail.com>
2025-04-25 20:17:33 +0530

    refactor(db.ts) : update db configure code

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,9 +3,7 @@ import mongoose from "mongoose";
 
 const connectDatabase = async() => {
     try {
-        const db = await mongoose.connect(process.env.DATABASE_URL as string, {
-            dbName:"AcreMate"
-        });
+        const db = await mongoose.connect(process.env.DATABASE_URL as string);
         console.log("Database...");
     } catch (error) {
         console.log(error);
